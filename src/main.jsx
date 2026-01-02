666da093ff38ed0aad15a75ea70d6f8cb921665b
kuanhsuehlo <woody40408@gmail.com>
2026-01-02 19:47:21 +0800

    update 0102

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // 嚴謹模式
   <StrictMode>
     <App />
   </StrictMode>,
