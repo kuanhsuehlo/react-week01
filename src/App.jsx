639e48a9051bf7e46bfe3f92377a6a7af04aacb9
kuanhsuehlo <woody40408@gmail.com>
2026-01-05 14:17:38 +0800

    update0106

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ function App() {
           <h2>產品細節</h2>
           {tempProduct ? (
             <div className="card mb-3">
-              <img src={tempProduct.imageUrl} className="card-img-top primary-image" alt="主圖" />
+              <img src={tempProduct.imageUrl} className="card-img-top primary-image" alt={tempProduct.title} />
               <div className="card-body">
                 <h5 className="card-title">
                   {tempProduct.title}
@@ -109,7 +109,7 @@ function App() {
                 <h5 className="mt-3">更多圖片：</h5>
                 <div className="d-flex flex-wrap">
                    {tempProduct.imagesUrl.map((url, index) => (
-                    <img key={index} src={url} className="images" />
+                    <img key={index} src={url} className="images" alt={`${tempProduct.title} - ${index + 1}`}/>
                   ))}
                 </div>
               </div>
